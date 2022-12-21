de406392decd86f51e616d1228e9b8ae6ca53e5b
Sorin Barbu <sbarbu38@yahoo.com>
2022-12-21 21:22:39 +0200

    Done 1.3

--- Catalog/src/Main.java
+++ Catalog/src/Main.java
@@ -1,7 +1,18 @@
+import CatalogCourses.*;
 import CatalogUsers.*;
 public class Main {
     public static void main(String[] args) {
-        User u = UserFactory.getUser(UserFactory.UserType.Parent, "a", "b");
-        System.out.println(u.getClass());
+        Grade g1 = new Grade();
+        g1.setPartialScore(2.3);
+        Grade g2 = null;
+        try {
+            g2 = (Grade) g1.clone();
+        } catch (CloneNotSupportedException e) {
+            System.out.println("problem");
+        }
+        g1.setExamScore(3.4000000000002);
+        g2.setExamScore(3.4000000000001);
+        System.out.println(g1.compareTo(g2));
     }
+
 }
